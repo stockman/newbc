59cc086eb626402d97785d904a13897b143113a0
Stickman <stocktonmanners@yahoo.ca>
2014-10-03 15:13:09 -0600

    better mobile pop outs

--- js/newbc.behaviors.js
+++ js/newbc.behaviors.js
@@ -1,60 +1,34 @@
-(function ($) {
+(function($) {
 
-  /**
-   * The recommended way for producing HTML markup through JavaScript is to write
-   * theming functions. These are similiar to the theming functions that you might
-   * know from 'phptemplate' (the default PHP templating engine used by most
-   * Drupal themes including Omega). JavaScript theme functions accept arguments
-   * and can be overriden by sub-themes.
-   *
-   * In most cases, there is no good reason to NOT wrap your markup producing
-   * JavaScript in a theme function.
-   */
-  Drupal.theme.prototype.newbcExampleButton = function (path, title) {
-    // Create an anchor element with jQuery.
-    return $('<a href="' + path + '" title="' + title + '">' + title + '</a>');
-  };
 
-  /**
-   * Behaviors are Drupal's way of applying JavaScript to a page. In short, the
-   * advantage of Behaviors over a simple 'document.ready()' lies in how it
-   * interacts with content loaded through Ajax. Opposed to the
-   * 'document.ready()' event which is only fired once when the page is
-   * initially loaded, behaviors get re-executed whenever something is added to
-   * the page through Ajax.
-   *
-   * You can attach as many behaviors as you wish. In fact, instead of overloading
-   * a single behavior with multiple, completely unrelated tasks you should create
-   * a separate behavior for every separate task.
-   *
-   * In most cases, there is no good reason to NOT wrap your JavaScript code in a
-   * behavior.
-   *
-   * @param context
-   *   The context for which the behavior is being executed. This is either the
-   *   full page or a piece of HTML that was just added through Ajax.
-   * @param settings
-   *   An array of settings (added through drupal_add_js()). Instead of accessing
-   *   Drupal.settings directly you should use this because of potential
-   *   modifications made by the Ajax callback that also produced 'context'.
-   */
-  Drupal.behaviors.newbcExampleBehavior = {
-    attach: function (context, settings) {
-      // By using the 'context' variable we make sure that our code only runs on
-      // the relevant HTML. Furthermore, by using jQuery.once() we make sure that
-      // we don't run the same piece of code for an HTML snippet that we already
-      // processed previously. By using .once('foo') all processed elements will
-      // get tagged with a 'foo-processed' class, causing all future invocations
-      // of this behavior to ignore them.
-      $('.some-selector', context).once('foo', function () {
-        // Now, we are invoking the previously declared theme function using two
-        // settings as arguments.
-        var $anchor = Drupal.theme('newbcExampleButton', settings.myExampleLinkPath, settings.myExampleLinkTitle);
 
-        // The anchor is then appended to the current element.
-        $anchor.appendTo(this);
-      });
-    }
-  };
+    Drupal.behaviors.myBehavior = {
+        attach: function(context, settings) {
+            //code starts
+            // for the user profiles
+            /*     $(".pane-user-login .form-submit").click(function() {
+                $(".form-item-name, .form-item-pass, .pane-user-login ul").toggle();
+            });
 
+*/
+            // toggles both user menus by usermenu class
+                $(".fa-search").click(function() {             
+                    $(".dl-menuwrapper button").removeClass('dl-active');
+                    $("#rm-dl-menu").removeClass('dl-menuopen');
+                    $(".followpop").hide();
+                    $(".search_form").toggle();
+//                    alert("add http://www.w3schools.com/jquerymobile/tryit.asp?filename=tryjqmob_forms_clear");                    
+                    // you already added the data-btn=true to the input
+                    //might be able now to just add jquery mobile.
+
+                });
+                $(".fa-share-square-o").click(function() {
+                    $(".dl-menuwrapper button").removeClass('dl-active');
+                    $("#rm-dl-menu").removeClass('dl-menuopen');
+                    $(".search_form").hide();
+                    $(".followpop").toggle();
+                });
+
+        }
+    };
 })(jQuery);
