73584a8c30da871d63ec480e61fb690e771e114f
Stickman <stocktonmanners@yahoo.ca>
2014-11-19 10:58:25 -0700

    better responsive logo

---
+++ newbc.behaviors.js
@@ -0,0 +1,16 @@
+(function($) {
+
+
+
+    Drupal.behaviors.myBehavior = {
+        attach: function(context, settings) {
+            //code starts
+
+            /** Fitttex.js sexlector sizes **/
+            $("h2").fitText(1.2, { minFontSize: '15px', maxFontSize: '50px' });
+            $("#fittext1").fitText();
+            $("#fittext2").fitText(1.2);
+            $("#fittext3").fitText(1.1, { minFontSize: '50px', maxFontSize: '75px' });
+        }
+    };
+})(jQuery);
