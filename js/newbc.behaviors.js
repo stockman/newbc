9c9e6d3e3dc22583d84ebcba49aa8d6aeb44b3b8
Stickman <stocktonmanners@yahoo.ca>
2014-11-20 18:29:38 -0700

    seasons, tags

--- js/newbc.behaviors.js
+++ js/newbc.behaviors.js
@@ -11,6 +11,14 @@
             $("#fittext1").fitText();
             $("#fittext2").fitText(1.2);
             $("#fittext3").fitText(1.1, { minFontSize: '50px', maxFontSize: '75px' });
+
+
+            // this is for the table sorter
+//             $("#permissions").dataTable().columnFilter();
+
+
+
+
         }
     };
 })(jQuery);
