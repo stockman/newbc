ac37d29363f8c566265c10d664e59ac29fe7b981
stocktonmanners <stocktonmanners@yahoo.ca>
2015-02-07 16:05:31 -0700

    last

--- js/newbc.behaviors.js
+++ js/newbc.behaviors.js
@@ -18,7 +18,27 @@
                  resetter.click(function() {
                    $(this).parent().find('input[type=text]').val("");
             });
+        // use this as selector changer?
+        //outside nav
+        /* strategy for future mashing into drupal?
+        jquery to add the new selectors (typoraility)
+        in this case outer/inner, menu and content.
+        if will be faster than adding them to parts.
+        once you have it figured you could add it?
+            or is there a better way to mass change selectors with drupal?
 
+        */
+        /*
+        on the resp off outer is not used for anything I think
+
+        */
+        
+        //inside all
+        $(".sitecontainer-inside").attr('id', 'inner-wrap');
+        //nav
+        $("#bettermenus").attr('id', 'nav');
+        $("#bettermenus").attr('role', 'navigation');
         }
+        
     };
 })(jQuery);
